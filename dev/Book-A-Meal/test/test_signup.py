1569179aa9ce19445c7941d8a9ba71e27bff3fc4
Esther <enmukungu@gmail.com>
2018-04-25 16:03:45 +0300

    tests page for signup

---
+++ dev/Book-A-Meal/test/test_signup.py
@@ -0,0 +1,11 @@
+import unittest
+from app.signup import signup
+
+class MainTest(unittest.TestCase):
+    
+    def test_signup(self): #how to implement functionality
+        
+        
+        
+if __name__ == "__main__":
+    unittest.main()
