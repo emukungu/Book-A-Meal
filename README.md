3f1cdee70f48f721e0cb46357c914e108461f646
Esther <enmukungu@gmail.com>
2018-04-30 16:44:29 +0300

    Description of challenge2 expectations

--- README.md
+++ README.md
@@ -2,17 +2,12 @@
 
 Book-A-Meal is an application that allows customers to make food orders and helps the food vendor know what the customers want to eat
 
-Required Features
-Users can create an account and log in
-Admin (Caterer) should be able to manage (i.e: add, modify and delete) meal options in the application. Examples of meal options are: Beef with rice, Beef with fries etc
-Admin (Caterer) should be able to setup menu for a specific day by selecting from the meal options available on the system.
-Authenticated users (customers) should be able to see the menu for a specific day and select an option out of the menu.
-Authenticated users (customers) should be able to change their meal choice.
-Admin (Caterer) should be able to see the orders made by the user
-Admin should be able to see amount of money made by end of day
+Challenge 2
 
-Extra Features 
-Authenticated users (customers) should be able to see their order history
-Authenticated users (customers) should be able to get notifications when the menu for the day has been set.
-Admin (Caterer) should be able to see order history
-The application should be able to host more than one caterer.
+Write tests for the functions and API endpoints
+
+Create API endpoints
+
+Create and use classes to model the entities and relationships
+
+Test API endpoints with Postman
--- dev/Book-A-Meal/app/views.py
+++ dev/Book-A-Meal/app/views.py
@@ -1,54 +1,26 @@
-from flask import (
-    jsonify, request, redirect,
-    url_for, render_template
-)
-from app import app, user_list
-from .models import User
+from flask import jsonify, request
+from app import app, user_list, meal_list
+from .models import User, Meals
 
-# these are controller functions
-
-@app.route("/auth/signup", methods= ['POST'])
+@app.route("/auth/signup", methods=['POST'])
 def signup():
-    #return jsonify({'message' : 'it works'})
-    #authentication
-    if request.method == 'POST':
-        data = request.json  # get the post data from request 
-        # initalise response
-        response = {
-            "status": "fail",
-            "message": "user not created"
-        }
-        # extract data from request and store it in local vars
-        username = data.get('username', None)
-        password = data.get('password', None)
-
-        if username != None and password != None or username != "" and password != "":  # always ensure username and password are provided
-            user_list.append(User(username=username, password=password))  # create an instance of user and add it to user_list
-            response['message'] = "user created"
-            response['status'] = "ok"
-            print(user_list)
-
-        return jsonify(response)
-
-
-@app.route("/auth/login", methods= ['POST'])
-def login():
-    data = request.json  # get the post data from request 
-    # initalise response
     response = {
-        "status": "fail",
-        "message": "user failed to login"
+        'message':''
     }
-    # extract data from request and store it in local vars
-    username = data.get('username', None)
-    password = data.get('password', None)
+    data = request.get_json()
+    firstName = data['firstName']
+    lastName = data['lastName']
+    password = data['password']
+    
+    if firstName == '' and lastName == '' and password == '':
+        response = {
+            'message': "yeeee"
+        }
+    else:
+        user = User(firstName=firstName, lastName=lastName, password=password)
+        user_list.append(user)
+        response['message'] = 201
 
-    if username != None and password != None or username != "" and password != "":
-        for user in user_list:
-            if user._username == username and user._password == password:
-                # hooray! we have user
-                response['message'] = "user logged in sucessfully"
-                response['status'] = "ok"
-                break
+    return jsonify(response)
             
-        return jsonify(response)
+            
