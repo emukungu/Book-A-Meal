9bd874453751750646336b89e3f9ba4436ed6fc4
Esther <enmukungu@gmail.com>
2018-04-30 16:26:32 +0300

    python module

--- dev/Book-A-Meal/app/__init__.py
+++ dev/Book-A-Meal/app/__init__.py
@@ -4,5 +4,10 @@ app = Flask(__name__)
 
 # This will hold all users created on signup
 user_list = []
+meal_list = []
+menu_list = []
+orders_list = []
+list_meals = []
+restaurant_list = []
+from . import views
 
-from app import views
