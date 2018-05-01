9e323e0694539d6bec24f98c8315c6393c0919a5
Esther <enmukungu@gmail.com>
2018-05-01 23:46:52 +0300

    class modelling users

--- dev/Book-A-Meal/app/models.py
+++ dev/Book-A-Meal/app/models.py
@@ -13,45 +13,13 @@ class User(object):
     def password(self):
         return self._password
 
-
-class Meals(object): #get all meals
-    def __init__(self, meal_name = None, price = None,):
-        self._meal_name = meal_name
-        self._price = price
+class User_logged_in(object):
+    def __init__(self, username=None, password = None):
+        self._username = username
+        self._password = password
     
-    
-    def meal_name(self):
-        return self._meal_name
-    
-    def price(self):
-        return self._price
-        
+    def username(self):
+        return self._username
 
-class Menu(object):
-    def __init__(self, name=None, items=[]):
-        self._name = name
-        self._items = items
-
-    def menu(self): #structure in which data is stored
-        return {
-            "name": self._name,
-            "items": self._items
-        }
-
-class Orders(object):
-    def __init__(self, items = []):
-        self._items = items
-        self._total = 0.0
-
-    def orders(self):
-        return self._items
-    
-    def total(self):
-        return self._total
-
-class Restaurants(object):
-    def __init__(self, name):
-        self._name = name
-
-    def name(self):
-        return self._name
+    def password(self):
+        return self._password
